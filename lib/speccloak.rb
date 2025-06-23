14dd8e3d1c8cac56ef3e75d71ecb7e48f74c2a7b
nitinrajkumarparuchuri <nitinrajkumar502@gmail.com>
2025-06-23 17:40:36 +0000

    first commit

---
+++ lib/speccloak.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "speccloak/version"
+
+module Speccloak
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/speccloak_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Speccloak do
+  it "has a version number" do
+    expect(Speccloak::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
